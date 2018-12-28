4311a363aa574452e91bc53b13db145425f2f84b
lboue <lboue@users.noreply.github.com>
2018-12-28 22:52:23 +0100

    Update phone.py

--- freepybox/api/phone.py
+++ freepybox/api/phone.py
@@ -3,19 +3,26 @@ class Phone:
     def __init__(self, access):
         self._access = access
 
+        
+    def get_status(self):
+        '''
+        Get phone status:
+        '''
+        return self._access.get('phone/')
+
 
     def get_config(self):
         '''
         Get phone configuration:
         '''
-        return self._access.get('phone/')
+        return self._access.get('phone/config/')
 
 
     def set_config(self, conf):
         '''
         Update phone configuration:
         '''
-        self._access.put('phone/', conf)
+        self._access.put('phone/config/', conf)
 
 
     def get_dect_vendors(self):
